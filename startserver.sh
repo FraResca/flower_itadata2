48e9d101a77fd07ee42e4e51625b02471e5e6ee1
FraResca <francesco.resca@edu.unife.it>
2025-05-23 12:05:54 +0200

    Sistemato per i mega esperimenti

--- startserver.sh
+++ startserver.sh
@@ -2,4 +2,4 @@
 
 export CUDA_VISIBLE_DEVICES=0
 # CUDA_LAUNCH_BLOCKING=1 python3 flserver.py
-CUDA_LAUNCH_BLOCKING=1 python3 newflserver.py $1 $2 > server$1$2 2>&1 &; disown
+CUDA_LAUNCH_BLOCKING=1 python3 newflserver.py $1 $2 > server$1$2 2>&1 &; disown $!
